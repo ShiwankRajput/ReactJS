74dfb5dabb405ca4e08f3a7c42bc4591821a602c
Shiwank <shiwannnnk@gmail.com>
2025-03-16 15:29:47 +0530

    fetching data using useEffect hook

--- 8_AdvanceReact/Social-Media-Advance/src/components/PostLists.jsx
+++ 8_AdvanceReact/Social-Media-Advance/src/components/PostLists.jsx
@@ -1,24 +1,82 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 function PostLists(){
 
    let {postList, addServerPost} = useContext(PostList);
 
-   let handleGetPostClick = ()=>{
-    fetch('https://dummyjson.com/posts')
+   let [fetching, setFetching] = useState(false);
+
+
+    //using useEffect hook cleanup when user moves to another component
+    useEffect(()=>{
+
+        const controller = new AbortController;
+        const signal = controller.signal;
+
+        setFetching(true);
+        fetch('https://dummyjson.com/posts',{signal})
+        .then(res => res.json())
+        .then(obj => {
+            addServerPost(obj.posts)
+            setFetching(false);
+        });
+
+
+        return ()=>{
+            controller.abort();
+        }
+
+    },[]);
+
+
+   //using useEffect  ---->
+    /*useEffect(()=>{
+
+        setFetching(true);
+        fetch('https://dummyjson.com/posts')
+        .then(res => res.json())
+        .then(obj => {
+            addServerPost(obj.posts)
+            setFetching(false);
+        });
+
+    }, []);*/
+
+
+   // this is done using state changed managemenet
+   /*let [dataFetched, setDataFetched] = useState(false);
+
+   if(!dataFetched){
+        fetch('https://dummyjson.com/posts')
         .then(res => res.json())
         .then(obj => addServerPost(obj.posts));
+
+        setDataFetched(true);
+    }*/
+
+
+   
+   
+   //fetch  api here is handle using button -->
+
+   let handleGetPostClick = ()=>{
+    // fetch('https://dummyjson.com/posts')
+    //     .then(res => res.json())
+    //     .then(obj => addServerPost(obj.posts));
    }
 
     return(
         <>
 
-            {postList.length == 0 && <WelcomeMessage onGetPostsClick={handleGetPostClick}></WelcomeMessage>}
+            {fetching && <LoadingSpinner></LoadingSpinner>}
+
+            {!fetching && postList.length == 0 && <WelcomeMessage onGetPostsClick={handleGetPostClick}></WelcomeMessage>}
         
-            {postList.map((post)=> {
+            {!fetching && postList.map((post)=> {
                 return <Post key={post.id} post={post} />
             })}
         
