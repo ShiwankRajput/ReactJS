01eef85ef0e280bf7ca207825dbf362599c1ccec
Shiwank <shiwannnnk@gmail.com>
2025-03-30 00:17:22 +0530

    basics of redux learning

--- 10_Redux/1_only_Redux/basicRedux.js
+++ 10_Redux/1_only_Redux/basicRedux.js
@@ -1,44 +1,41 @@
-const redux = require('redux');
+const redux = require('redux')
 
 const INITIAL_VALUE = {
-    counter : 0
+    counter: 0
 }
 
-//reducer
-const reducer = (store = INITIAL_VALUE,action)=>{
+const reducer = (state = INITIAL_VALUE,action)=>{
+    let newStore = state;
 
-    let newStore = store;
-
-    if(action.type === "INCREMENT"){
-        newStore = {counter: newStore.counter+1};
+    if(action.type === 'INCREMENT'){
+        newStore = {counter: state.counter+1}
     }
-    else if(action.type === "DECREMENT"){
-        newStore = {counter: newStore.counter-1};
+    else if(action.type === 'DECREMENT'){
+        newStore = {counter: state.counter-1}
     }
-    else if(action.type === "ADDITION"){
-        newStore = {counter: newStore.counter + action.payload.number}
-    }
-    else if(action.type === "SUBTRACTION"){
-        newStore = {counter: newStore.counter - action.payload.number}
+    else if(action.type === 'ADD'){
+        newStore = {counter: state.counter + action.payload.num}
     }
 
     return newStore;
     
 }
 
-//store
 const store = redux.createStore(reducer)
 
-//subscribe
 const subscriber = ()=>{
-    const state = store.getState()
+    const state = store.getState();
     console.log(state);
 }
 
-store.subscribe(subscriber);
+store.subscribe(subscriber)
 
-//action
-store.dispatch({type: "INCREMENT"})
-store.dispatch({type: "DECREMENT"})
-store.dispatch({type: "ADDITION", payload: {number: 5}})
-store.dispatch({type: "SUBTRACTION", payload: {number: 3}})
+store.dispatch({type:'INCREMENT'});
+store.dispatch({type:'INCREMENT'});
+store.dispatch({type:'DECREMENT'});
+store.dispatch({type:'INCREMENT'});
+store.dispatch({type:'INCREMENT'});
+store.dispatch({type:'ADD',payload:{num: 4}});
+
+
+
